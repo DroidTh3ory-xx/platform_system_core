LineageOS/android_system_core_old

libion: Support for old ION API
 * Kernel 3.0 doesn't have heap_mask or SYNC ioctl, so add a board flag
   so that proprietary code that uses the old kernel API directly still
   works.

Change-Id: I4227c79a5de46c60eeebd2e16ca88dd6d580916a

libion/Android.mk
@@ -3,6 +3,9 @@ ifneq ($(TARGET_BOARD_PLATFORM),omap3)
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+ifeq ($(BOARD_USE_OLD_ION_API),true)
+LOCAL_CFLAGS += -DOLD_ION_API
+endif
 LOCAL_SRC_FILES := ion.c
 LOCAL_MODULE := libion
 LOCAL_MODULE_TAGS := optional

DroidTh3ory-xx/platform_system_core

libion/Android.mk
@@ -1,6 +1,9 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+ifeq ($(BOARD_HAVE_OLD_ION_API),true)
+LOCAL_CFLAGS += -DOLD_ION_API
+endif
 LOCAL_SRC_FILES := ion.c
 LOCAL_MODULE := libion
 LOCAL_MODULE_TAGS := optional